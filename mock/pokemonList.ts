c224a1ce5110ce8b353d1842339d0595cccb8995
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-17 14:57:14 +0200

    chore(mock): create mock data for implementing UI

---
+++ mock/pokemonList.ts
@@ -0,0 +1,64 @@
+import type { PokemonListItem } from "@/types/pokemon";
+
+export const mockPokemonList: PokemonListItem[] = [
+    {
+        name: "bulbasaur",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "charmander",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+        name: "squirtle",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+        name: "pikachu",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+        name: "jigglypuff",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/39.png",
+    },
+    {
+        name: "meowth",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/52.png",
+    },
+    {
+        name: "eevee",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png",
+    },
+    {
+        name: "mewtwo",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+    },
+    {
+        name: "mew",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+    },
+    {
+        name: "umbreon",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/197.png",
+    },
+    {
+        name: "scizor",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/212.png",
+    },
+    {
+        name: "suicune",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/245.png",
+    },
+];
