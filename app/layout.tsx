f35dd876220b2f4de68c99479495d8f9e265f833
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-19 18:31:24 +0200

    chore(images): change images to webp

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Image from "next/image";
-import logo from "@/public/logo.png";
+import logo from "@/public/logo.webp";
 import ThemeToggle from "@/components/ThemeToggle";
 import Link from "next/link";
 
@@ -41,6 +41,7 @@ export default function RootLayout({
                           src={logo}
                           width={1021}
                           height={334}
+                          priority
                           alt="logo"
                           className="select-none w-full max-w-[800px] h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer" // Dodano "cursor-pointer" dla wskaźnika
                       />
