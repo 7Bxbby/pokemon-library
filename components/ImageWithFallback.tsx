f8dd215303aad4270f5cc2808420453dc9ed5676
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-19 11:16:21 +0200

    feat(ImageWithFallback): create ImageWithFallback for easier empty image validation

---
+++ components/ImageWithFallback.tsx
@@ -0,0 +1,58 @@
+import {useState} from "react";
+import Image from "next/image";
+
+export default function ImageWithFallback({
+    id,
+    src,
+    alt,
+    width,
+    height,
+    }: {
+    id: number;
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+}) {
+    const [imageUrl, setImageUrl] = useState(src);
+    const [attempts, setAttempts] = useState(2);
+    const imageFallback = "https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/0.png";
+    const fallbackHome = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`;
+
+    const handleError = () => {
+        setAttempts(prev => {
+            const next = prev - 1;
+
+            if (next === 1) {
+                setImageUrl(fallbackHome);
+            } else if (next <= 0) {
+                setImageUrl(imageFallback);
+            }
+            return next;
+        });
+    };
+
+
+    return (
+        <div
+            className="relative inline-block group"
+        >
+            <Image
+                src={imageUrl}
+                width={width}
+                height={height}
+                alt={alt}
+                onError={handleError}
+                loading="lazy"
+                className="relative z-10 transition-transform group-hover:-translate-y-3 duration-500
+                    drop-shadow-[0_4px_8px_rgba(0,0,0,0.3)]
+                    dark:drop-shadow-[0_4px_8px_rgba(255,255,255,0.1)]"
+            />
+            <span
+                className={`${imageUrl === imageFallback ? "group-hover:opacity-100 group-hover:translate-y-0" : ""} opacity-0 translate-y-2 pointer-events-none absolute bottom-0 left-1/2 transform -translate-x-1/2 px-4 py-2 text-white text-sm rounded-lg shadow-xl whitespace-nowrap transition-all duration-500`}
+            >
+                No Image available
+            </span>
+        </div>
+    );
+}
--- components/PokemonListDisplay.tsx
+++ components/PokemonListDisplay.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import { PokemonListItem } from "@/types/pokemon";
-import {useState} from "react";
+import ImageWithFallback from "@/components/ImageWithFallback";
 
 type Props = { pokemonList: PokemonListItem[] };
 
@@ -19,45 +18,17 @@ export default function PokemonListDisplay({ pokemonList }: Props) {
                 <div className="absolute bottom-10 w-24 h-6 bg-gray-900/50 dark:bg-gray-100/20 rounded-full blur-md group-hover:bg-gray-900/30 dark:group-hover:bg-gray-100/50 transition-all duration-500" />
 
                 <ImageWithFallback
+                    id={pokemon.id}
                     src={pokemon.image}
                     alt={pokemon.name}
                     width={180}
                     height={180}
                 />
 
-                <p className="mt-2 text-white font-bold capitalize drop-shadow-md text-xl">
-                    {pokemon.name.replace('-', ' ')}
+                <p className="mt-2 text-white font-bold text-center capitalize drop-shadow-md text-xl">
+                    {pokemon.name}
                 </p>
             </div>
         </Link>
     ));
-}
-
-function ImageWithFallback({
-                               src,
-                               alt,
-                               width,
-                               height,
-                           }: {
-    src: string;
-    alt: string;
-    width: number;
-    height: number;
-}) {
-    const [imageError, setImageError] = useState(false);
-
-    return (
-        <Image
-            src={imageError ? "/pokemon-placeholder.png" : src}
-            width={width}
-            height={height}
-            alt={alt}
-            onError={() => setImageError(true)}
-            loading="lazy"
-            sizes="(max-width: 768px) 33vw, 180px"
-            className="relative z-10 transition-transform group-hover:-translate-y-3 duration-500
-                 drop-shadow-[0_4px_8px_rgba(0,0,0,0.3)]
-                 dark:drop-shadow-[0_4px_8px_rgba(255,255,255,0.1)]"
-        />
-    );
 }
