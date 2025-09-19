9f7cbd2a0e7381ec351a4116420c55e3cb15cf3c
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-19 08:58:59 +0200

    chore(types): update PokemonListResponse type

--- types/pokemon.ts
+++ types/pokemon.ts
@@ -67,10 +67,8 @@ export interface PokemonListItem {
 
 export interface PokemonListResponse {
     count: number;
-    next: string | null;
-    previous: string | null;
-    results: Array<{
+    pokemons: Array<{
         name: string;
-        url: string;
+        image: string;
     }>;
 }
