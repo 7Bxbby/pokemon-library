0935ed80c447e1a189ab5c68341c38f43dd1654a
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-19 08:59:44 +0200

    chore(utils): export variable pokeapi base url

--- lib/pokemon-utils.ts
+++ lib/pokemon-utils.ts
@@ -1,6 +1,6 @@
 import {PokemonListItem} from "@/types/pokemon";
 
-const POKEAPI_BASE_URL = 'https://pokeapi.co/api/v2';
+export const POKEAPI_BASE_URL = 'https://pokeapi.co/api/v2';
 
 export function parsePokemonList(data: { results: { name: string; url: string }[] }): PokemonListItem[] {
     return data.results.map((p) => {
@@ -10,22 +10,5 @@ export function parsePokemonList(data: { results: { name: string; url: string }[
             image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
         };
     });
-}
-
-export async function fetchPokemonListFromPokeAPI(offset: number) {
-    const response = await fetch(`${POKEAPI_BASE_URL}/pokemon?limit=12&offset=${offset}`, {
-        next: { revalidate: 3600 }
-    });
-
-    if (!response.ok) {
-        throw new Error(`PokeAPI error: ${response.status}`);
-    }
-
-    const data: { count: number; results: { name: string; url: string }[] } = await response.json();
-
-    return {
-        count: data.count,
-        pokemons: parsePokemonList(data),
-    };
 }
 
