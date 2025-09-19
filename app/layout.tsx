90d81e7206e15bdbbc48c15e830377c4e7a69e73
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-19 14:50:19 +0200

    feat(logo-redirect): add redirect to home page on logo click

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/contexts/ThemeContext";
 import Image from "next/image";
 import logo from "@/public/logo.png";
 import ThemeToggle from "@/components/ThemeToggle";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,13 +36,16 @@ export default function RootLayout({
       <ThemeProvider>
           <div className="min-h-screen poke-bg">
               <div className="text-4xl justify-center items-center h-[334px] px-4 pt-12 relative">
-                  <Image
-                      src={logo}
-                      width={1021}
-                      height={334}
-                      alt="logo"
-                      className="select-none w-full max-w-[800px] h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
+                  <Link href="/">
+                      <Image
+                          src={logo}
+                          width={1021}
+                          height={334}
+                          alt="logo"
+                          className="select-none w-full max-w-[800px] h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer" // Dodano "cursor-pointer" dla wskaźnika
+                      />
+                  </Link>
+
                   <ThemeToggle />
               </div>
               {children}
