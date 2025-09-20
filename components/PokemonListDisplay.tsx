40c44ae56d82ec2c205d77b177455e421095c784
7Bxbby <bxbbyafterhours@gmail.com>
2025-09-20 15:15:27 +0200

    feat(styles): add loading styles when opening pokemon details page

--- components/PokemonListDisplay.tsx
+++ components/PokemonListDisplay.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
-import {PokemonListItem} from "@/types/pokemon";
-import ImageWithFallback from "@/components/ImageWithFallback";
+import {PokemonListItem as PokemonListItemType} from "@/types/pokemon";
 import Image from "next/image";
 import ErrorImage from "@/public/pokemon-placeholder.webp";
 import React from "react";
+import PokemonListItem from "@/components/PokemonListItem";
 
 type Props = {
-    pokemonList: PokemonListItem[],
+    pokemonList: PokemonListItemType[],
     clearSearch: () => void
 };
 
@@ -23,30 +22,6 @@ export default function PokemonListDisplay({pokemonList, clearSearch}: Props) {
             </button>
         </div>
     return pokemonList.map((pokemon, i) => (
-        <Link
-            href={`/pokemon/${pokemon.name}`}
-            className="flex flex-col justify-center items-center cursor-pointer"
-            key={pokemon.name}
-        >
-            <div
-                className="group relative flex flex-col items-center fade-in-up"
-                style={{animationDelay: `${i * 100}ms`}}
-            >
-                <div
-                    className="absolute bottom-10 w-24 h-6 bg-gray-900/50 dark:bg-gray-100/20 rounded-full blur-md group-hover:bg-gray-900/30 dark:group-hover:bg-gray-100/50 transition-all duration-500"/>
-
-                <ImageWithFallback
-                    id={pokemon.id}
-                    src={pokemon.image}
-                    alt={pokemon.name}
-                    width={180}
-                    height={180}
-                />
-
-                <p className="mt-2 text-white font-bold text-center capitalize drop-shadow-md text-xl">
-                    {pokemon.name}
-                </p>
-            </div>
-        </Link>
+        <PokemonListItem key={i} pokemon={pokemon} index={i}/>
     ));
 }
